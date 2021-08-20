329f8640b830c699d52e5b69ab9b92dc05aaab8a
shycoder <thatshycoder@gmail.com>
2021-08-20 03:04:11 +0100

    Fixed v2 bug

--- includes/admin.php
+++ includes/admin.php
@@ -113,5 +113,8 @@ class EMCS_Admin
         add_option('emcs_stop_review_notice', 0);
         add_option('emcs_stop_newsletter_notice', 0);
         add_option('emcs_display_greeting', 1);
+
+        require_once(EMCS_EVENT_TYPES . 'event-types.php');
+        EMCS_Event_Types::create_emcs_event_types_table();
     }
 }
